0df6afad95ee4d1e460c8d96d376305d1c7e11bd
David Gemmell <2559087G@student.gla.ac.uk>
2022-03-24 12:25:04 +0000

    index images now take users to the post they refer to

--- static/js/rerigBase.js
+++ static/js/rerigBase.js
@@ -14,12 +14,17 @@ function goToAccount(username) {
     window.location = '/rerig/account/'+ username;
 }
 
+function goToPost(id) {
+    console.log(id)
+    window.location = '/rerig/post/' + String(id);
+}
+
 function goToLogin() {
     window.location = '/rerig/login/';
 }
 
 function logout() {
-    window.location = '/rerig/logout'
+    window.location = '/rerig/logout';
 }
 
 function register() {
@@ -27,5 +32,5 @@ function register() {
 }
 
 function goToAddPost() {
-    window.location = '/rerig/add_post'
+    window.location = '/rerig/add_post';
 }
