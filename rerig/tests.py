b1cece538d0e4bef46f278fa26ff10dc51b5233e
“Mariusz <2551634s@student.gla.ac.uk>
2022-03-25 16:34:05 +0000

    added index test

--- rerig/tests.py
+++ rerig/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_posts(self):
+        """
+        If no post exists, the appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('rerig:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no posts present.')
+        self.assertQuerysetEqual(response.context['posts'], [])
+    
+
+
+
+
 
 # Create your tests here.
